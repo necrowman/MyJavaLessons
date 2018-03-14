54323a445d7f7f18ee7ebca5fd90e2bce455eb3a
Ruslan Yupyn <ruslan.yupyn@inveritasoft.com>
2018-03-14 21:46:18 +0200

    First version of Fibonachi number counting;

---
+++ MyJavaLessons/src/com/necrowman/FibonachiClass.java
@@ -0,0 +1,20 @@
+package com.necrowman;
+
+public class FibonachiClass{
+    public long fibonachi(int f){
+        if(f < 0) {
+            return 0;
+        }
+        else if(f <= 1){
+            return 1;
+        }
+        else {
+            int f1 = f - 1;
+            int f2 = f - 2;
+            long fib1 = fibonachi(f1);
+            long fib2 = fibonachi(f2);
+            return fib1 + fib2;
+        }
+    }
+
+}
--- MyJavaLessons/src/com/necrowman/Main.java
+++ MyJavaLessons/src/com/necrowman/Main.java
@@ -1,8 +1,18 @@
 package com.necrowman;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        FibonachiClass obj = new FibonachiClass();
+        System.out.println("Fibonachi (-7) is: " + obj.fibonachi(-7));
+        System.out.println("Fibonachi (0) is: " + obj.fibonachi(0));
+        System.out.println("Fibonachi (1) is: " + obj.fibonachi(1));
+        System.out.println("Fibonachi (2) is: " + obj.fibonachi(2));
+        System.out.println("Fibonachi (3) is: " + obj.fibonachi(3));
+        System.out.println("Fibonachi (5) is: " + obj.fibonachi(5));
+        System.out.println("Fibonachi (7) is: " + obj.fibonachi(7));
+        System.out.println("Fibonachi (10) is: " + obj.fibonachi(10));
+        System.out.println("Fibonachi (45) is: " + obj.fibonachi(45));
     }
 }
